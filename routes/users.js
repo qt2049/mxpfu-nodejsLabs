1dd3e70cfc243bbd66f7b12bdddb64cb297a93e5
Your Name <you@example.com>
2025-10-28 12:46:33 -0400

    wip

--- routes/users.js
+++ routes/users.js
@@ -116,8 +116,12 @@ router.put("/:email", (req, res) => {
 
 // DELETE request: Delete a user by email ID
 router.delete("/:email", (req, res) => {
-  // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+    // Extract the email parameter from the request URL
+    const email = req.params.email;
+    // Filter the users array to exclude the user with the specified email
+    users = users.filter((user) => user.email != email);
+    // Send a success message as the response, indicating the user has been deleted
+    res.send(`User with the email ${email} deleted.`);
 });
 
 module.exports=router;
